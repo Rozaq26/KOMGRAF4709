3d34afd2af67b2e6803b1d1fe2664a8246fcdbde
RzqKuliah <115638267+Rozaq26@users.noreply.github.com>
2023-11-29 18:59:30 +0700

    Update Translasi2.1.py

--- HW7/Translasi2.1.py
+++ HW7/Translasi2.1.py
@@ -2,29 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def translate(matrix, translation):
-    """
-    Mentranslasi matriks dengan vektor translasi.
-
-    Args:
-        matrix: Matriks yang akan ditranslasi.
-        translation: Vektor translasi.
-
-    Returns:
-        Matriks yang telah ditranslasi.
-    """
+   
     translated_matrix = np.empty_like(matrix)
     for i in range(matrix.shape[0]):
         translated_matrix[i] = matrix[i] + translation
     return translated_matrix
 
 def connect_points(points, color):
-    """
-    Menghubungkan titik-titik dengan garis, termasuk menghubungkan titik awal dan akhir.
-
-    Args:
-        points: List of tuples (x, y) yang merupakan koordinat titik-titik.
-        color: Warna garis.
-    """
+   
     x_values = [p[0] for p in points]
     y_values = [p[1] for p in points]
     plt.plot(x_values, y_values, '-', color=color)
@@ -33,25 +18,11 @@ def connect_points(points, color):
     plt.plot([x_values[0], x_values[-1]], [y_values[0], y_values[-1]], '-', color=color)
 
 def plot_points(points, color='black'):
-    """
-    Menampilkan grafik titik-titik.
-
-    Args:
-        points:  (x, y) yang merupakan koordinat titik-titik.
-        color: Warna titik (default: hitam).
-    """
+    
     plt.plot([p[0] for p in points], [p[1] for p in points], 'o', color=color)
 
 def plot_grid(xmin, xmax, ymin, ymax):
-    """
-    Menampilkan grid pada grafik.
-
-    Args:
-        xmin: Koordinat x minimum.
-        xmax: Koordinat x maksimum.
-        ymin: Koordinat y minimum.
-        ymax: Koordinat y maksimum.
-    """
+    
     plt.xticks(np.arange(xmin, xmax + 1))
     plt.yticks(np.arange(ymin, ymax + 1))
     plt.grid(True)
